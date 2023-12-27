54c4868c971f4035d20f091146ced3fb103d779e
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2023-12-27 19:21:04 +0600

    changes

--- src/components/NavigationBox.jsx
+++ src/components/NavigationBox.jsx
@@ -152,7 +152,7 @@ const NavigationBox = () => {
             )}
 
             {searchValue !== "" && (
-                <div className="mt-[4.5rem] overflow-y-auto scrollbarHide h-[calc(100vh-4.5rem)] pb-[20rem]">
+                <div className="mt-[4.5rem] overflow-y-auto scrollbarHide h-[calc(100vh-4.5rem)] pb-[16rem]">
                     <ul
                         className={`${
                             isClick ? "hidden" : "flex"
